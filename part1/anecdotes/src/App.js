2c48b3efd69af533389288e6bfab1c0c6ed56e13
Arvindh Manian <arvindh.manian@duke.edu>
2022-10-04 23:32:53 -0400

    Finish 1.12-1.14

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const MaxScorer = ({anecdotes, points}) => {
+  const max_points = Math.max(...points)
+  const anecdote = anecdotes[points.indexOf(max_points)]
+  return (
+    <div>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdote}</p>
+      <p>has {max_points} votes</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Uint8Array(anecdotes.length))
+
+  const nextAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const vote = (selected) => {
+    let copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <Button onClick={nextAnecdote} text="next anecdote"></Button>
+      <Button onClick={() => vote(selected)} text="vote"></Button>
+      <p>has {points[selected]} point(s)</p>
+      <MaxScorer anecdotes={anecdotes} points={points}></MaxScorer>
+    </div>
+  )
+}
+
+export default App
