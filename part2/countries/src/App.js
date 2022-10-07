c53ba44d528f5223ce0edaab29a8b7da8f4aa6bc
Arvindh Manian <arvindh.manian@duke.edu>
2022-10-06 22:38:48 -0400

    Finish exercises 2.11-2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -7,11 +7,17 @@ import Filter from './components/Filter'
 const App = () => {
     const [currFilter, setFilter] = useState('')
     const [countries, setCountries] = useState([])
+    const [countriesToShow, setCountriesToShow] = useState([])
+
+    useEffect(() => setCountriesToShow(countries.filter((country) => country.name.common.toLowerCase().includes(currFilter))), [currFilter, countries])
 
     const filterController = (event) => {
         event.preventDefault()
-        console.log(event.target.value)
         setFilter(event.target.value)
+    }
+
+    const buttonHandler = (country) => {
+        setCountriesToShow([country])
     }
 
     useEffect(() => {
@@ -22,11 +28,9 @@ const App = () => {
             })
     }, [])
 
-    const countriesToShow = countries.filter((country) => country.name.common.toLowerCase().includes(currFilter))
-
     return <div>
         <Filter callback={filterController}></Filter>
-        <CountriesDisplay countries={countriesToShow}></CountriesDisplay>
+        <CountriesDisplay callback={buttonHandler} countries={countriesToShow}></CountriesDisplay>
     </div>
     
 }
--- part2/countries/src/components/CountriesDisplay.js
+++ part2/countries/src/components/CountriesDisplay.js
@@ -1,4 +1,9 @@
-const CountriesDisplay = ({ countries }) => {
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+const REACT_APP_WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+
+const CountriesDisplay = ({ countries, callback }) => {
     if (countries.length === 0) {
         return <p>No countries found</p>
     }
@@ -8,21 +13,38 @@ const CountriesDisplay = ({ countries }) => {
     }
 
     if (countries.length > 1) {
-        return <CountryList countries={countries}></CountryList>
+        return <CountryList callback={callback} countries={countries}></CountryList>
     }
     
     return <Country country={countries[0]}></Country>
 }
 
-const CountryList = ({ countries }) => {
+const CountryList = ({ countries, callback }) => {
+    const showButtonHandler = (country) => {
+        return () => callback(country)
+    }
+
     return <div>
-        {countries.map((country) => <p>{country.name.common}</p>)}
-    </div>
+    {countries.map((country) => 
+        <div key={country.name.common}>
+            <p>{country.name.common}</p>
+            <button onClick={showButtonHandler(country)}>show more</button>
+        </div>
+    )}</div>
+
 }
 
 
 const Country = ({ country }) => {
-    console.log(country)
+    const [weatherData, setWeatherData] = useState(-1)
+
+    useEffect(() => {axios
+                    .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]},${country.cca3}&appid=${REACT_APP_WEATHER_API_KEY}`)
+                    .then(
+                        (response) => {setWeatherData(response.data)}
+                    )}, 
+                [country.capital, country.cca3]
+            )
 
     return (<div>
         <h1>{country.name.common}</h1>
@@ -33,7 +55,23 @@ const Country = ({ country }) => {
             {Object.entries(country.languages).map((language) => <li key={language[1]}>{language[1]}</li>)}
         </ul>
         <img src={country.flags.png} alt="flag"></img>
+        {<Weather weather={weatherData}></Weather>}
     </div>)
+}
+
+const Weather = ({ weather }) => {
+    if (weather === -1) {
+        return <></>
+    }
+
+    return (
+        <div>
+            <h1>Weather in {weather.name}</h1>
+            <p>temperature {Math.round((weather.main.temp - 273) * 100) / 100} Celcius</p>
+            <img alt="" src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}></img>
+            <p>wind {weather.wind.speed} m/s</p>
+        </div>
+    )
 }
 
 export default CountriesDisplay
